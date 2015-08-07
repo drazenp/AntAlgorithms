0e7e0849bc9e5993050def0db8f5609adf367bcf
drazenp86 <drazenp86@gmail.com>
2015-08-07 22:32:38 +0200

    Included graph files.

---
+++ Basic/Aspg.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic
+{
+    public class Aspg
+    {
+        //private 
+        //public ASPG(TC, TG, MaxBrIteracija, BrRegiona, alfa, beta, ro, tol)
+        //{
+                
+        //}
+    }
+}
