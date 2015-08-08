9fcfebbc47b9079ec856421b148bbaea0e3fa0cc
drazenp86 <drazenp86@gmail.com>
2015-08-08 08:23:56 +0200

    Solved reading vertices weights.

--- AntAlgorithms/BasicTests/GraphTests.cs
+++ AntAlgorithms/BasicTests/GraphTests.cs
@@ -1,4 +1,5 @@
-﻿using Basic;
+﻿using System.IO;
+using Basic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BasicTests
@@ -11,6 +12,36 @@ namespace BasicTests
         {
             var graph = new Graph("baba.txt", "baba.txt");
 
+            using (var stream = new MemoryStream())
+            using (var writer = new StreamWriter(stream))
+            {
+                writer.Write(@"tc
+                                4
+                                3
+                                8
+                                7
+                                5
+                                3
+                                6
+                                7
+                                2");
+                writer.Flush();
+                stream.Position = 0;
+                
+                graph.ReadVerticesWeights(stream);
+            }
+
+            Assert.AreEqual(9, graph.VerticesWeights.Count);
+        }
+
+        private Stream GenerateStreamFromString(string s)
+        {
+            MemoryStream stream = new MemoryStream();
+            StreamWriter writer = new StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
         }
     }
 }
