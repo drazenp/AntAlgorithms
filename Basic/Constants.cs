a7c0beb86f42a2272f35380f9c31a1bde1aafbdc
drazenp86 <drazenp86@gmail.com>
2015-08-15 14:56:41 +0200

    Commited contstansts for first time.

---
+++ Basic/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace Basic
+{
+    public class Constants
+    {
+        // Initial value of pheromone on graph. C
+        public static readonly decimal MinimalValueOfPheromone = 0.00001M;
+    }
+}
