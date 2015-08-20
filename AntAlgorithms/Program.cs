e233de5da4b493355bfc266bc6aac5c965e76086
drazenp86 <drazenp86@gmail.com>
2015-08-20 21:00:50 +0200

    Fist test

--- AntAlgorithms/Program.cs
+++ AntAlgorithms/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace AntAlgorithms
+﻿using Basic;
+
+namespace AntAlgorithms
 {
     class Program
     {
+        public const string _basicEdges = "B1.txt";
+        public const string _basicVeretxWeights = "B2.txt";
+
         static void Main(string[] args)
         {
+            var options = new Options(numberOfIterations: 100, numberOfRegions: 3, alfa: 1, beta: 5, ro: 0.6, delta: 0.1D);
+            var graph = new Graph(_basicEdges, _basicVeretxWeights);
+            var aspg = new Aspg(options, graph);
+            aspg.GetQuality();
         }
     }
 }
