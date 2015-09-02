62021906f0c1fae2ddd9465ac7fba1d523072f7a
drazenp86 <drazenp86@gmail.com>
2015-09-02 20:28:29 +0200

    Added two new project for other algorithms.

---
+++ ParallelOptimisation/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelOptimisation
+{
+    public class Class1
+    {
+    }
+}
