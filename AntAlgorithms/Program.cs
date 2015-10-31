a65ffda2c67b9a3a639f9bf56f7d27f559827093
drazenp <drazenp86@gmail.com>
2015-10-31 08:10:25 +0100

    Update main test.

--- AntAlgorithms/Program.cs
+++ AntAlgorithms/Program.cs
@@ -19,20 +19,20 @@ namespace AntAlgorithms
             graph.InitializeGraph();
             var rnd = new Random(Environment.TickCount);
             var aspg = new Aspg(options, graph, rnd);
-            aspg.GetQuality();
+            var resultBasic = aspg.GetQuality();
 
             var parallelOptimisationOptoins = 
                 new OptionsParallelOptimisation(numberOfIterations: 100, numberOfRegions: 3, alfa: 1, beta: 5, ro: 0.6, delta: 0.1D, numberOfInterSections:5);
             graph = new Graph(BasicEdges, BasicVertexWeights);
             graph.InitializeGraph();
             var aspgParallelOptimisation = new AspgParallelOptimisation(parallelOptimisationOptoins, graph, rnd);
-            aspgParallelOptimisation.GetQuality();
+            var resultParallelOptimisation = aspgParallelOptimisation.GetQuality();
 
             graph = new Graph(BasicEdges, BasicVertexWeights);
             graph.InitializeGraph();
             var aspgParallelOptimisationWithInheritance = new AspgParallelOptimisationWithInheritance(parallelOptimisationOptoins,
                 graph, rnd);
-            aspgParallelOptimisationWithInheritance.GetQuality();
+            var resultParallelOptimisationWithInheritance = aspgParallelOptimisationWithInheritance.GetQuality();
         }
     }
 }
