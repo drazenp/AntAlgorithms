dc29f19822ca174b7deda970b41a50d08fbbf53e
drazenp <drazenp86@gmail.com>
2016-03-13 22:21:12 +0100

    Small improvement on Issue #14: resultParallelOptimisationWithInheritance is not working at all.

--- AntAlgorithms/Program.cs
+++ AntAlgorithms/Program.cs
@@ -19,23 +19,26 @@ namespace AntAlgorithms
         {
             log4net.Config.XmlConfigurator.Configure();
 
-            var options = new Options(numberOfIterations: 100, numberOfRegions: 3, alfa: 1, beta: 5, ro: 0.6, delta: 0.1D);
+            var rnd = new Random(Environment.TickCount);
+
+            //var options = new Options(numberOfIterations: 10, numberOfRegions: 3, alfa: 1, beta: 5, ro: 0.6, delta: 0.1D);
+            //var graph = new Graph(BasicEdges, BasicVertexWeights);
+            //graph.InitializeGraph();
+            //var aspg = new Aspg(options, graph, rnd);
+            //var resultBasic = aspg.GetQuality();
+
+            //var parallelOptimisationOptoins =
+            //    new OptionsParallelOptimisation(numberOfIterations: 10, numberOfRegions: 3, alfa: 1, beta: 5, ro: 0.6, delta: 0.1D, numberOfInterSections: 5);
+            //graph = new Graph(BasicEdges, BasicVertexWeights);
+            //graph.InitializeGraph();
+            //var aspgParallelOptimisation = new AspgParallelOptimisation(parallelOptimisationOptoins, graph, rnd);
+            //var resultParallelOptimisation = aspgParallelOptimisation.GetQuality();
+
+            var parallelOptimisationWithInheritanceOptoins =
+                new OptionsParallelOptimisation(numberOfIterations: 10, numberOfRegions: 3, alfa: 1, beta: 5, ro: 0.6, delta: 0.1D, numberOfInterSections: 5);
             var graph = new Graph(BasicEdges, BasicVertexWeights);
             graph.InitializeGraph();
-            var rnd = new Random(Environment.TickCount);
-            var aspg = new Aspg(options, graph, rnd);
-            var resultBasic = aspg.GetQuality();
-
-            var parallelOptimisationOptoins =
-                new OptionsParallelOptimisation(numberOfIterations: 100, numberOfRegions: 3, alfa: 1, beta: 5, ro: 0.6, delta: 0.1D, numberOfInterSections: 5);
-            graph = new Graph(BasicEdges, BasicVertexWeights);
-            graph.InitializeGraph();
-            var aspgParallelOptimisation = new AspgParallelOptimisation(parallelOptimisationOptoins, graph, rnd);
-            var resultParallelOptimisation = aspgParallelOptimisation.GetQuality();
-
-            graph = new Graph(BasicEdges, BasicVertexWeights);
-            graph.InitializeGraph();
-            var aspgParallelOptimisationWithInheritance = new AspgParallelOptimisationWithInheritance(parallelOptimisationOptoins,
+            var aspgParallelOptimisationWithInheritance = new AspgParallelOptimisationWithInheritance(parallelOptimisationWithInheritanceOptoins,
                 graph, rnd);
             var resultParallelOptimisationWithInheritance = aspgParallelOptimisationWithInheritance.GetQuality();
         }
